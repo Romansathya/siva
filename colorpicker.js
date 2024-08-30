442c76a730061bfa73161ecf5ddcbd4d3dfc9f56
TALARI SIVASATHYANARAYANA <138603101+Romansathya@users.noreply.github.com>
2024-08-30 11:40:18 +0530

    Add files via upload

---
+++ colorpicker.js
@@ -0,0 +1,22 @@
+let backgroundEl=document.getElementById("background");
+let para=document.getElementById("para");
+
+function first(){
+    backgroundEl.style.backgroundColor="red";
+    para.style.color="green"
+    
+}
+function second(){
+    backgroundEl.style.backgroundColor="green";
+    para.style.color="red"
+} 
+function third(){
+    backgroundEl.style.backgroundColor="blue";
+    para.style.color="yellow"
+    
+}
+function fourth(){
+    backgroundEl.style.backgroundColor="yellow";
+    para.style.color="blue"
+   
+}
